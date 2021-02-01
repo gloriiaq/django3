288a145e64b670ee4af3e02771c0754ed886c1a1
gloriiaq <gloriaqian2020@gmail.com>
2021-02-01 11:03:24 -0500

    intial web1

--- web1/app01/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-
-# Create your views here.
-def home1(request):
-    return HttpResponseRedirect("/static/index.html")
