e800310636bf6607b6740bc7fc98e6fc7facbf71
gloriiaq <gloriaqian2020@gmail.com>
2021-02-01 19:10:38 -0500

    setup dj03

--- web2/web2/urls.py
+++ web2/web2/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app03 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home.as_view(), name='home'),
+    path('', Home.as_view(), name='home'),
 ]
